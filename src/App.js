af90032f38a6d2451b945210f0470f53161f7d2b
BORA JEONG <brjeong@sphinfo.co.kr>
2023-11-09 17:59:06 +0900

    Fisrt Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "monday-ui-react-core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+
+      <div>
+        <Button onClick={function noRefCheck(){}}>
+          1개의 연결된 보드
+        </Button>
+        <div className="test-div">테스트 중입니다.</div>
+      </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'monday-ui-react-core/tokens';
+import './resources/scss/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
